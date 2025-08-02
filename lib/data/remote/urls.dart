91b99c9e39b491013be511e20c0c0d19482c0426
sahebbali <saheb1996jmr@gmail.com>
2025-08-02 09:55:17 +0600

    add gemini base url

---
+++ lib/data/remote/urls.dart
@@ -0,0 +1,8 @@
+class Urls {
+  static const String geminiBaseUrl =
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent";
+  static const String geminiApiKey = "AIzaSyBUIu3wUE_tx860Ry2vvMl5MzOvfp9MZyA"; // Store securely
+}
+
+/// here we change model
+/// /// change your api key also if any api key related problem message  message
